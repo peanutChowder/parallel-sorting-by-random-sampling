b50494624da8e29f330ec227b426cbadc4029cb2
Jacob Feng <peanutchowder@gmail.com>
2025-01-31 09:04:58 -0700

    Added data generator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::time::Instant;
+use rand::Rng;
+
+fn generate_data(n: usize, start: u32, end: u32) -> Box<[u32]> {
+    let timeStart = Instant::now();
+    let mut data: Box<[u32]> = vec![0; n].into_boxed_slice();
+    let mut rng = rand::rng();
+
+    for i in 0..n {
+        data[i] = rng.random_range(start..end);
+    }
+
+    let duration = timeStart.elapsed();
+    println!("Time elapsed: {:?}", duration);
+
+    return data;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let data = generate_data(10_000_000, 0, 50);
 }

9c40917aa75683b9188f29a0142d6e5f942fcbe2
Jacob Feng <peanutchowder@gmail.com>
2025-02-11 10:35:52 -0700

    Completed up to phase 3, changed threading to use crossbeam

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
-use std::sync::{Arc, Mutex};
-use std::thread;
-use std::thread::JoinHandle;
+use std::cell::UnsafeCell;
+use std::sync::{Arc, Barrier, Mutex};
 use std::time::Instant;
 use rand::Rng;
 
+use crossbeam::thread;
+use std::cmp::Ordering;
+
 const LOG_SPECIFIC: bool = false;
+const ARRAY_LEN: usize = 10_000;
+const P: usize = 10;
 
 fn generate_data(n: usize, start: u32, end: u32) -> Box<[u32]> {
     let time_start = Instant::now();
@@ -44,27 +48,69 @@ fn thread_quicksort(data: &mut [u32], low: usize, high: usize) {
     }
 }
 
-fn psrs(data: &mut [u32], p: usize) {
-    let block_len = data.len() / p;
-    let mut handles: Vec<std::thread::JoinHandle<()>>  = vec![];
+fn psrs(data: &mut [u32]) {
+    let block_len = ARRAY_LEN / P;
+    // let mut handles: Vec<std::thread::JoinHandle<()>>  = vec![];
+    let global_samples = Arc::new(UnsafeCell::new([0u32; P * P]));
+    let pivots = Arc::new(UnsafeCell::new([0u32; P - 1]));
+    const OMEGA: usize = ARRAY_LEN / (P * P);
 
-    for chunk in data.chunks_mut(block_len) {
-        let mut chunk = chunk.to_vec();
-        let chunk_len = chunk.len();
-        let handle = thread::spawn(move || {
-            thread_quicksort(&mut chunk[..], 0, chunk_len - 1);
-        });
-        handles.push(handle);
-    }
+    thread::scope(|scope| {
+        let barrier = Arc::new(Barrier::new(P));
+        let pivot_clone = Arc::clone(&pivots);
+        for thread_id in 0..P {
+            let barrier_clone = barrier.clone();
+            let start_idx = thread_id * block_len;
+            let end_idx = if thread_id == P - 1 {
+                data.len()
+            } else {
+                (thread_id + 1) * block_len
+            };
+            let chunk = &mut data[start_idx..end_idx];
+            let global_samples_clone = Arc::clone(&global_samples);
 
-    for handle in handles {
-        handle.join().unwrap();
-    }
+            scope.spawn(move |_| {
+                // Phase 1: quicksort in parallel on chunks
+                thread_quicksort(chunk, 0, chunk.len() - 1);
+                barrier_clone.wait();
+
+                // Phase 2: Obtain regular sample in parallel
+                let global_samples_ref = unsafe { &mut *global_samples_clone.get()};
+                for i in 0..P {
+                    let j = i * OMEGA + 1;
+                    if j < chunk.len() {
+                        global_samples_ref[thread_id * P + i] = chunk[j];
+                    }
+                }
+                barrier_clone.wait();
+
+
+                if thread_id == 0 {
+                    // Phase 2: A dedicated thread sorts the regular sample
+                    thread_quicksort(global_samples_ref, 0, global_samples_ref.len() - 1);
+                    let pivot_ref = unsafe { &mut *pivots.get()};
+
+                    // Phase 2: dedicated thread collects the pivots
+                    for i in 0..P {
+                        pivot_ref[i - 1] = global_samples_ref[i * (global_samples_ref.len() / P)];
+                    }
+                }
+                barrier_clone.wait();
+
+                // Phase 3: bins are created based on the pivots
+                let mut partitions: Vec<Vec<u32>> = vec![vec![]; P];
+                let pivot_ref = unsafe { &mut *pivots.get()};
+
+
+
+            });
+        }
+    }).unwrap();
 }
 
 fn verify_sorted(data: &[u32]) -> bool {
     let mut is_valid: bool = true;
-    for i in 0..(data.len() - 1) {
+    for i in 0..(ARRAY_LEN - 1) {
         if data[i] > data[i + 1] {
             if LOG_SPECIFIC {
                 println!("INVALID entry at indices data[{:?}] and data[{:?}]: values {:?} and {:?}", i, i + 1, data[i], data[i + 1]);
@@ -76,7 +122,6 @@ fn verify_sorted(data: &[u32]) -> bool {
 }
 
 fn main() {
-    let array_len = 100_000;
     let mut warm_ups = 1;
 
     for i in 0..4 {
@@ -86,10 +131,10 @@ fn main() {
             println!("---------------------------");
             println!("Run #{}", i - warm_ups);
         }
-        let mut data = generate_data(array_len, 0, 50);
+        let mut data = generate_data(ARRAY_LEN, 0, 50);
 
         let start = Instant::now();
-        psrs(&mut data, 5);
+        psrs(&mut data);
         let duration = start.elapsed();
         println!("Time elapsed in quicksort: {:?}", duration);
 

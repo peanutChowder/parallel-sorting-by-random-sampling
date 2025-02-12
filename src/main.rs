4cb67c4dfbe59afd0ea4a44db5a26757dc41a3e5
Jacob Feng <peanutchowder@gmail.com>
2025-02-11 17:02:43 -0700

    Completed general implementation

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use std::cell::UnsafeCell;
-use std::sync::{Arc, Barrier, Mutex};
+use std::cmp::Reverse;
+use std::sync::{Arc, Barrier};
 use std::time::Instant;
 use rand::Rng;
 
 use crossbeam::thread;
-use std::cmp::Ordering;
+
+use std::collections::BinaryHeap;
 
 const LOG_SPECIFIC: bool = false;
 const ARRAY_LEN: usize = 10_000;
@@ -50,14 +52,14 @@ fn thread_quicksort(data: &mut [u32], low: usize, high: usize) {
 
 fn psrs(data: &mut [u32]) {
     let block_len = ARRAY_LEN / P;
-    // let mut handles: Vec<std::thread::JoinHandle<()>>  = vec![];
     let global_samples = Arc::new(UnsafeCell::new([0u32; P * P]));
     let pivots = Arc::new(UnsafeCell::new([0u32; P - 1]));
+    let partitions = Arc::new(UnsafeCell::new(vec![vec![Vec::new(); P]; P]));
+    let partition_lens = Arc::new(UnsafeCell::new(vec![0; P]));
     const OMEGA: usize = ARRAY_LEN / (P * P);
 
     thread::scope(|scope| {
         let barrier = Arc::new(Barrier::new(P));
-        let pivot_clone = Arc::clone(&pivots);
         for thread_id in 0..P {
             let barrier_clone = barrier.clone();
             let start_idx = thread_id * block_len;
@@ -68,6 +70,8 @@ fn psrs(data: &mut [u32]) {
             };
             let chunk = &mut data[start_idx..end_idx];
             let global_samples_clone = Arc::clone(&global_samples);
+            let global_partitions_clone = Arc::clone(&partitions);
+            let partition_lens_clone = Arc::clone(&partition_lens);
 
             scope.spawn(move |_| {
                 // Phase 1: quicksort in parallel on chunks
@@ -97,12 +101,58 @@ fn psrs(data: &mut [u32]) {
                 }
                 barrier_clone.wait();
 
-                // Phase 3: bins are created based on the pivots
-                let mut partitions: Vec<Vec<u32>> = vec![vec![]; P];
+                // Phase 3: partitions in each thread chunk are created based on the pivots
+                // let mut partitions: Vec<Vec<u32>> = vec![vec![]; P];
                 let pivot_ref = unsafe { &mut *pivots.get()};
 
+                let mut prev = 0;
+                let global_partitions_ref = unsafe { &mut *global_partitions_clone.get()};
+                for (i, &pivot_val) in pivot_ref.iter().enumerate() {
+                    let ind = match chunk.binary_search(&pivot_val) {
+                        Ok(idx) => idx + 1,
+                        Err(idx) => {idx}
+                    };
+                    global_partitions_ref[thread_id][i].extend_from_slice(&chunk[prev..ind]);
+                    prev = ind;
+                }
+                global_partitions_ref[thread_id][P - 1] = chunk[prev..chunk.len()].to_vec();
+                barrier_clone.wait();
 
+                // Part 4: Create a min heap for a k-way merge within each thread
+                let mut min_heap = BinaryHeap::new();
+                for (partitionIndex, partition) in global_partitions_ref.iter().enumerate() {
+                    if !partition.is_empty() {
+                        // Each thread 'thread_id' picks the first element from every other
+                        // thread's partition in order to start the k-way merge
+                        min_heap.push(Reverse((partition[thread_id][0], partitionIndex, 0)));
+                    }
+                }
 
+                // Part 4: Begin the k-way merge
+                let mut final_local_partition = Vec::new();
+                let partition_lens_ref = unsafe { &mut *partition_lens_clone.get()};
+                while let Some(Reverse((val, partitionIndex, prevIndex))) = min_heap.pop() {
+                    // We use partitionIndex to track which sub-vector we just popped from,
+                    // so we must push in the next element in that sub-vector to our min heap, IF
+                    // it isn't empty
+                    final_local_partition.push(val);
+                    if prevIndex + 1 < global_partitions_ref[partitionIndex][thread_id].len() {
+                        min_heap.push(Reverse((global_partitions_ref[partitionIndex][thread_id][prevIndex + 1], partitionIndex, prevIndex + 1)));
+                    }
+                }
+                partition_lens_ref[thread_id] = final_local_partition.len();
+                barrier_clone.wait();
+
+                let mut offset = 0;
+                if thread_id > 0 {
+                    for i in 0..thread_id {
+                        offset += partition_lens_ref[i];
+                    }
+                }
+
+                for i in 0..final_local_partition.len() {
+                    data[offset + i] = final_local_partition[i];
+                }
             });
         }
     }).unwrap();
